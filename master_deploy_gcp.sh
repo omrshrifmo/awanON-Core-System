74906ae76e22e7f32d873cffc2bf7e5bbd076af5
omrshrifmo <omrshrifmo@gmail.com>
2025-06-16 20:37:38 +0300

    Update master_deploy_gcp.sh

--- master_deploy_gcp.sh
+++ master_deploy_gcp.sh
@@ -151,7 +151,7 @@ gcloud run deploy "${TSWIQON_AGENT_SERVICE_NAME}" \
   --min-instances=0 \
   --max-instances=1 \
   --execution-environment=gen2 \
-  --startup-probe \
+  --startup-probe=path=/healthz,timeout-seconds=600 \
   --no-cpu-throttling \
   --allow-unauthenticated \
   --quiet
